884c12aa1706d475e2b56f80aad8b392ef3c90ef
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2021-04-06 23:12:45 +0900

    UserRowテストのリファクタリング

--- tests/unit/components/UserRow.spec.ts
+++ tests/unit/components/UserRow.spec.ts
@@ -26,53 +26,62 @@ const adminUser: User = {
   isAdmin: true,
 };
 
-describe("UserRow.vue", () => {
-  it("有効なユーザーのテスト", () => {
-    const wrapper = mount(UserRow, {
-      props: {
-        user: testUser,
-        onDeleteBtnClick: jest.fn(),
-      },
-    });
-
+describe("UserRowに有効なユーザーが渡されたとき", () => {
+  const wrapper = mount(UserRow, {
+    props: {
+      user: testUser,
+      onDeleteBtnClick: jest.fn(),
+    },
+  });
+  it("渡されたデータが正常に描画されること", () => {
     const tdList = wrapper.findAll("td");
 
     expect(tdList[0].text()).toBe(testUser.id.toString());
     expect(tdList[1].text()).toBe(testUser.name);
     expect(tdList[2].text()).toBe(testUser.mail);
-
+  });
+  it("disabledスタイルが適用されていないこと", () => {
+    expect(wrapper.classes()).not.toContain("disabled");
+  });
+  it("ボタンが非活性でないこと", () => {
     const deleteBtn = wrapper.find("button");
     expect(deleteBtn.attributes()).not.toHaveProperty("disabled");
-
-    expect(wrapper.classes()).not.toContain("disabled");
-
+  });
+  it("ボタンクリック時にonDeleteBtnClickがコールされること", () => {
+    const deleteBtn = wrapper.find("button");
     deleteBtn.trigger("click");
     expect(wrapper.vm.onDeleteBtnClick).toHaveBeenCalled();
   });
+});
 
-  it("管理者ユーザーのテスト", () => {
-    const wrapper = mount(UserRow, {
-      props: {
-        user: adminUser,
-        onDeleteBtnClick: jest.fn(),
-      },
-    });
+describe("UserRowに無効なユーザーが渡されたとき", () => {
+  const wrapper = mount(UserRow, {
+    props: {
+      user: disableUser,
+      onDeleteBtnClick: jest.fn(),
+    },
+  });
+  it("disabledスタイルが適用されていること", () => {
+    expect(wrapper.classes()).toContain("disabled");
+  });
+});
 
+describe("UserRowに管理者ユーザーが渡されたとき", () => {
+  const wrapper = mount(UserRow, {
+    props: {
+      user: adminUser,
+      onDeleteBtnClick: jest.fn(),
+    },
+  });
+  it("ボタンが非活性であること", () => {
     const deleteBtn = wrapper.find("button");
     expect(deleteBtn.attributes()).toHaveProperty("disabled");
+  });
+
+  it("ボタンをクリックしてもonDeleteBtnClickがコールされること", () => {
+    const deleteBtn = wrapper.find("button");
 
     deleteBtn.trigger("click");
     expect(wrapper.vm.onDeleteBtnClick).not.toHaveBeenCalled();
-  });
-
-  it("無効なユーザーにはスタイルが適用されること", () => {
-    const wrapper = mount(UserRow, {
-      props: {
-        user: disableUser,
-        onDeleteBtnClick: jest.fn(),
-      },
-    });
-
-    expect(wrapper.classes()).toContain("disabled");
   });
 });
