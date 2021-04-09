b8e3d871d622cb9854e095b8b4e31f165d1d098b
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2021-04-09 16:07:22 +0900

    テスト修正

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -2,15 +2,12 @@ import { mount } from "@vue/test-utils";
 import App from "@/App.vue";
 
 describe("App.vue", () => {
+  const wrapper = mount(App);
   it("初期描画ではcountが0であること", () => {
-    const wrapper = mount(App);
-
     expect(wrapper.vm.count).toBe(0);
   });
 
   it("クリック時にcountがインクリメントされること", () => {
-    const wrapper = mount(App);
-
     const button = wrapper.find("button");
     button.trigger("click");
     expect(wrapper.vm.count).toBe(1);
